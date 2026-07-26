a2dbdfe640b3dcf8878192bf2550a11d8529dc81
Ratul Polley <ratulpolley0@gmail.com>
2026-07-26 14:01:10 +0530

    Fix MongoDB connection

--- resturant-backend/config/db.js
+++ resturant-backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://ratulpolley450_db_user:kejNyEpYl3Orgt6q@ac-qzokxhb-shard-00-00.0lyakz0.mongodb.net:27017,ac-qzokxhb-shard-00-01.0lyakz0.mongodb.net:27017,ac-qzokxhb-shard-00-02.0lyakz0.mongodb.net:27017/?ssl=true&replicaSet=atlas-s24h6c-shard-0&authSource=admin&appName=Cluster0");
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB Connected Successfully ✅");
   } catch (error) {
